79461e5fe334186f6039bfff3715c15e0df3d66a
0scar5egovia <coach@osronlinefitness.com>
2025-06-09 11:38:30 +0100

    Update separator.tsx

--- app/components/ui/separator.tsx
+++ app/components/ui/separator.tsx
@@ -1,22 +1,18 @@
 import * as React from "react"
-import { cn } from "@/utils"
+import { cn } from "../../utils"
 
-export interface SeparatorProps
-  extends React.HTMLAttributes<HTMLDivElement> {}
+export interface SeparatorProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-const Separator = React.forwardRef<HTMLDivElement, SeparatorProps>(
-  ({ className, orientation = "horizontal", ...props }, ref) => (
+export function Separator({ className, orientation = "horizontal", ...props }: SeparatorProps) {
+  return (
     <div
-      ref={ref}
+      role="separator"
       className={cn(
         "shrink-0 bg-border",
-        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        orientation === "horizontal" ? "h-px w-full" : "h-full w-px",
         className
       )}
       {...props}
     />
   )
-)
-Separator.displayName = "Separator"
-
-export { Separator }
+}

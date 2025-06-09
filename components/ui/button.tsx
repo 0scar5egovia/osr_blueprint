75b0136f1c25bb9feb94e62befda3986a7c7ded9
0scar5egovia <coach@osronlinefitness.com>
2025-06-09 10:26:44 +0100

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,3 +1,5 @@
+FROM: components/ui/button.tsx
+TO:   app/components/ui/button.tsx
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/utils"

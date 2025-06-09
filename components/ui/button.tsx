8af84d24b169412922610b381a6b27c2c14b214c
0scar5egovia <coach@osronlinefitness.com>
2025-06-09 09:39:47 +0100

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,1 +1,12 @@
+import * as React from "react"
 
+export function Button({ children, ...props }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      className="px-4 py-2 bg-black text-white rounded hover:bg-gray-800 transition"
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
